a2c4a4c42d83b146d40e16634793076333f20639
majid shahid <majidshahid474@gmail.com>
2021-04-05 16:19:36 -0700

    nested loop

---
+++ app.js
@@ -0,0 +1,38 @@
+// var a ="";
+// var b="";
+// for (a=1;a<1000;a=a+100){
+//     for (b=a;b<a+100;b++){
+//         document.write(b+"<br>")
+//     }
+//     document.write("<br>")
+// }
+
+// for (var a=1;a<=5;a++){
+//     for(var b =1; b<=a;b++){
+//         document.write(b)
+//     }
+//     document.write("<br>")
+// }
+
+
+// for (var a=5;a>=1;a--){
+//     for (var b=1;b<=a;b++){
+//         document.write(a)
+//     }
+//     document.write("<br>")
+    
+// }
+
+
+// for (var a =5;a>=1;a--){
+//     for(var b =1;b<=a;b++){
+//         document.write(b)
+//     }
+//     document.write("<br>")
+// }
+
+
+// var a =5;
+// for (var b =1 ;b<=a;b++){
+// document.write(b)
+// }
