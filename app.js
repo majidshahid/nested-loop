d12f0709b7058d00a605ad95aabd8914e23c6e5c
majid shahid <majidshahid474@gmail.com>
2021-04-06 15:58:11 -0700

    loop

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-// var a ="";
-// var b="";
-// for (a=1;a<1000;a=a+100){
-//     for (b=a;b<a+100;b++){
-//         document.write(b+"<br>")
-//     }
-//     document.write("<br>")
-// }
+var a ="";
+var b="";
+for (a=1;a<100;a=a+10){
+    for (b=a;b<a+10;b++){
+        document.write(b)
+    }
+    document.write("<br>")
+}
 
-// for (var a=1;a<=5;a++){
-//     for(var b =1; b<=a;b++){
-//         document.write(b)
-//     }
-//     document.write("<br>")
-// }
+for (var a=1;a<=5;a++){
+    for(var b =1; b<=a;b++){
+        document.write(b)
+    }
+    document.write("<br>")
+}
 
 
-// for (var a=5;a>=1;a--){
-//     for (var b=1;b<=a;b++){
-//         document.write(a)
-//     }
-//     document.write("<br>")
+for (var a=5;a>=1;a--){
+    for (var b=1;b<=a;b++){
+        document.write(a)
+    }
+    document.write("<br>")
     
-// }
+}
 
 
-// for (var a =5;a>=1;a--){
-//     for(var b =1;b<=a;b++){
-//         document.write(b)
-//     }
-//     document.write("<br>")
-// }
+for (var a =5;a>=1;a--){
+    for(var b =1;b<=a;b++){
+        document.write(b)
+    }
+    document.write("<br>")
+}
 
 
-// var a =5;
-// for (var b =1 ;b<=a;b++){
-// document.write(b)
-// }
+var a =5;
+for (var b =1 ;b<=a;b++){
+document.write(b + "<br>")
+}
+
+for (var a=5;a>=1;a--){
+    for (var b=1;b<=a;b++){
+        document.write(b)
+    }
+    document.write("<br>")
+}
